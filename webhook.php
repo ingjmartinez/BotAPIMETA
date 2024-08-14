5fc99673a6b4a3438456f40235228e57e9bd2521
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-08-13 20:06:44 -0400

    subiendo05

--- webhook.php
+++ webhook.php
@@ -59,6 +59,16 @@
                 ]
             ]);
         }else{
+            $data = json_encode([
+                "messaging_product" => "whatsapp",    
+                "recipient_type" => "individual",
+                "to" => $numero,
+                "type" => "text",
+                "text" => [
+                    "preview_url" => false,
+                    "body" => "🚀 Hola, visita mi web anderson-bastidas.com para más información.\n \n📌Por favor, ingresa un número #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en pdf. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜"
+                ]
+            ]);
 
         }
 
